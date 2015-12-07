a18b8a27cb347380bb481a80dac51147f1b993f8
osmancicek <osman.cicek93@gmail.com>
2015-12-07 22:52:54 +0200

    deneme1

--- KolaySiparisApp/views.py
+++ KolaySiparisApp/views.py
@@ -2,8 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-     return render(request, 'home.html')
-
-def register(request):
-     return render(request,'register.html')
-     
+     return render(request, 'home.html'')
