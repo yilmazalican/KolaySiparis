fed8679ae7ea8b4e67182e48e9d34c23ec6e0704
yilmazalican <alican.yilmaz@isik.edu.tr>
2015-12-09 00:40:20 +0200

    added form of register page and remarked register.html

---
+++ KolaySiparisApp/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+
+class RegisterForm(forms.Form):
+    name = forms.CharField(label='Your name', max_length=100)
+    name = forms.CharField(label='Your name', max_length=100)
+    name = forms.CharField(label='Your name', max_length=100)
+    name = forms.CharField(label='Your name', max_length=100)
+    name = forms.CharField(label='Your name', max_length=100)
+    name = forms.CharField(label='Your name', max_length=100)
+    name = forms.CharField(label='Your name', max_length=100)
+    name = forms.CharField(label='Your name', max_length=100)
+    name = forms.CharField(label='Your name', max_length=100)
